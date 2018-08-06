642965bd8121c499447c6865b531f5ec514887c8
Haley Barlar <hbarlar1@gmail.com>
2018-08-06 14:31:15 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,33 @@
+require 'pry'
+
+cart = [
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"KALE"    => {:price => 3.0, :clearance => false}}
+]
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each_with_index do |hash|
+    hash.each do |food, food_data|
+      if new_hash[food] == nil
+        new_hash[food] = food_data.merge({:count => 1})
+      else
+        new_hash[food][:count] += 1 
+      end
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+#code here
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |food, food_data|
+    if 
+  end
 end
 
 def checkout(cart, coupons)
